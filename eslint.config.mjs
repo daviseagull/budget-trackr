ec3215793f3a2eded19abc6779829cb6815cbbe5
Davi Seagull <daviseagull@icloud.com>
2024-05-11 10:05:32 -0400

    chore: initialize monorepo

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import pluginJs from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default [
+  {
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    ignores: [
+      'coverage',
+      'public',
+      'dist',
+      'pnpm-lock.yaml',
+      'pnpm-workspace.yaml',
+    ],
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
