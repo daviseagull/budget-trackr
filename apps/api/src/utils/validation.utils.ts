5e41535f955e749f68b9d5eb7b6a83eb628b503b
Davi Seagull <daviseagull@icloud.com>
2024-05-17 08:36:42 -0400

    refactor: improve code of safeParse validation method

--- apps/api/src/utils/validation.utils.ts
+++ apps/api/src/utils/validation.utils.ts
@@ -4,10 +4,6 @@ export const validationUtils = {
   safeParse: <T>(data: unknown, schema: z.ZodSchema): T | undefined => {
     const result = schema.safeParse(data)
 
-    if (!result.success) {
-      return undefined
-    }
-
-    return result.data!
+    return result.success ? result.data : undefined
   },
 }
